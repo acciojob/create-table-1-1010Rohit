a51cab193226af46940ab69740e71fa474e86ea8
1010Rohit <parmarrohit.rp43@gmail.com>
2025-04-09 14:29:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
+	let x = document.getElementById('sampleTable');
+	x.innerHTML += `<tr><td>New cell1</td> 
+		<td>New cell2</td></tr> `
+	
   
   
 }
